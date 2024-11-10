5f69756de5954ac404c8e8e4081dff87f6a1a4dd
maeEsp <maemae.espera@gmail.com>
2024-11-10 09:27:36 +0800

    feat(gallery): add initial lookback data and images and hero section of Gallery page

---
+++ src/app/Gallery/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Gallery Page',
+  description: '',
+};
+
+export default function Home() {
+  return <main className='bg-blue pt-8'>
+    <div>
+
+    </div>
+  </main>;
+}
