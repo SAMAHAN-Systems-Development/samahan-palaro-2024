100742e9a97d3a9b7eb6f3c25111e9b69c2083a5
Mariel <marmabs692@gmail.com>
2024-11-26 17:48:05 +0800

    refactor: deleted py-8 in the parent div

--- src/components/ui/ClosingCeremony.tsx
+++ src/components/ui/ClosingCeremony.tsx
@@ -8,41 +8,35 @@ import Link from 'next/link';
 
 const ClosingCeremony = () => {
   return (
-    <div className="py-8">
-      <div className="mx-auto w-full max-w-full px-4 mt-12 mb-24">
-        {closingCeremonyData.map(({ imageUrl, title, link }, index) => (
-          <div key={index} className="flex flex-col w-full bg-blue shadow-lg">
-            <div className="relative w-full h-[358px] mt-24 border-24 border-8 border-pink">
-              <Image
-                src={imageUrl}
-                alt={title}
-                layout="fill"
-                sizes="(max-width: 768px) 100vw, 50vw"
-                className="object-cover"
-                priority
-              />
-            </div>
+    <div className="mx-auto w-full max-w-full mt-12">
+      {closingCeremonyData.map(({ imageUrl, title, link }, index) => (
+        <div key={index} className="flex flex-col w-full bg-blue shadow-lg">
+          <div className="relative w-full h-[358px] mt-24 border-24 border-8 border-pink">
+            <Image
+              src={imageUrl}
+              alt={title}
+              layout="fill"
+              sizes="(max-width: 768px) 100vw, 50vw"
+              className="object-cover"
+              priority
+            />
+          </div>
 
-            <div className="w-full p-8 px-48">
-              <div className="flex flex-col md:flex-row items-center justify-between mb-6">
-                <div className="font-jersey10 text-[60px] text-white text-right flex-1 mb-4 md:mb-0">
-                  {title}
-                </div>
+          <div className="w-full p-8 px-48">
+            <div className="flex flex-col md:flex-row items-center justify-between mb-6">
+              <div className="font-jersey10 text-[60px] text-white text-right flex-1 mb-4 md:mb-0">
+                {title}
+              </div>
 
-                <div className="flex justify-center md:ml-8">
-                  <Link href={link} passHref>
-                    <ReadMoreButton
-                      bgColor="green"
-                      textColor="pink"
-                      size="big"
-                    />
-                  </Link>
-                </div>
+              <div className="flex justify-center md:ml-8">
+                <Link href={link} passHref>
+                  <ReadMoreButton bgColor="green" textColor="pink" size="big" />
+                </Link>
               </div>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
