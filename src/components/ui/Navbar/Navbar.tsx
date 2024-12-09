858a672009d87be51ff8de3261554323f0681e1c
Mariel <marmabs692@gmail.com>
2024-12-09 13:11:53 +0800

    feat: fixed navbar responsiveness for smaller screens

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar: FC = () => {
 
   return (
     <nav className="jersey10 bg-blue shadow-lg border-b-8 border-green px-8">
-      <div className="w-full mx-auto flex justify-between items-center p-4">
+      <div className="w-full mx-auto flex justify-between items-center p-4 relative">
         {/* Mobile Menu Toggle */}
         <button
           onClick={() => setMenuOpen(!isMenuOpen)}
@@ -27,23 +27,20 @@ const Navbar: FC = () => {
 
         {/* Logo */}
         <Link href="/">
-          <Image
-            src={logo.src}
-            width={125}
-            height={125}
-            alt="Palaro Logo"
-            className="h-8"
-          />
+          <Image src={logo.src} width={150} height={125} alt="Palaro Logo" />
         </Link>
 
-        {/* Desktop Navigation Links */}
+        {/* Navigation Links */}
         <ul
-          className={`lg:flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-8 py-4 font-jersey10 ${isMenuOpen ? 'absolute top-[100px] left-0 w-full bg-blue shadow-lg' : 'hidden lg:flex z-'}`}
+          className={`lg:flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-8 absolute lg:relative ${
+            isMenuOpen ? 'top-full' : 'hidden'
+          } left-0 w-full lg:w-auto bg-blue lg:bg-transparent py-6 lg:py-0 px-6 lg:px-0`}
+          style={{ zIndex: 40 }}
         >
           {menuItems.map(({ href, label }) => (
             <li
               key={href}
-              className="hover:text-pink text-white transition-colors duration-300 text-2xl lg:text-4xl"
+              className="font-jersey10 text-white text-3xl hover:text-pink transition-colors"
             >
               <Link href={href} onClick={() => setMenuOpen(false)}>
                 {label}
